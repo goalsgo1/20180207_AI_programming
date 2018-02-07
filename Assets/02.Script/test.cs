0298ff460fb667013bc601f153fe22d75a53a59a
goalsgo1 <36186769+goalsgo1@users.noreply.github.com>
2018-02-07 10:40:28 +0900

    good

---
+++ Assets/02.Script/test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //good
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
